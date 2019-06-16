b16f7d863c7c59fb069129aed64f6d1b40da448e
Bill Kremer <bkrems12@gmail.com>
2019-06-15 21:51:28 -0500

    bodyParser is a part of express.js now

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var routes = require('./routes/routes.js');
-var mongoose = require('mongoose');
-mongoose.set('useCreateIndex', true);
 
+var mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true); // fixes deprecation warning
+mongoose.set('useFindAndModify', false); // fixes deprecation warning
 
 var app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 routes(app); // call the routes and send the app.
 
-/** --- MONGOOSE CONNECTION --- **/
+// Mongoose connection to MongoDB - Local or remote
 app.set('mongo_uri', process.env.MONGODB_URI || 'mongodb://localhost:27017/myretail');
 mongoose.connect(app.get('mongo_uri'), { useNewUrlParser: true });
 
